e06c8f1187e51af689ed8e369e9313ea51c7d24d
Marcelo Matos <marcelomatos.dev@gmail.com>
2020-03-20 15:51:17 -0500

    created routes and header

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'customers/index'
-  get 'customers/show'
-  get 'customers/alphabetized'
-  get 'customers/missing_email'
+  get '/customers', to: 'customers#index'
+  # get 'customers/show'
+
+  get '/alphabetized', to: 'customers#alphabetized'
+
+  get '/missing_email', to: 'customers#missing_email'
+
+  root to: 'customers#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
